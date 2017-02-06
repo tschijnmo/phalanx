05e4ed1b3ef8df6ff0acaa6fb3c8370839c6d29c
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2017-02-06 16:32:41 -0600

    Add setup script

---
+++ setup.py
@@ -0,0 +1,36 @@
+"""Setup script for phalanx."""
+
+from distutils.core import setup
+
+DESCRIPTION = \
+    'A simple parallel processing library based on MPI emulating Apache Spark'
+
+with open('README.rst', 'r') as readme:
+    LONG_DESCRIPTION = readme.read()
+
+CLASSIFIERS = [
+    'Development Status :: 1 - Planning',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3 :: Only',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Science/Research',
+    'Topic :: System :: Distributed Computing'
+]
+
+REQUIREMENTS = [
+    "mpi4py>=2.0.0",
+]
+
+setup(
+    name='phalanx',
+    version='0.1.0',
+    author='Jinmo Zhao and Gustavo E Scuseria',
+    author_email='tschijnmotschau@gmail.com',
+    packages=['phalanx'],
+    url='https://github.com/tschijnmo/phalanx',
+    license='LICENSE',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    classifiers=CLASSIFIERS,
+    install_requires=REQUIREMENTS,
+)
